cc749d67b2eb9edde5ce5180b2cb6950bf91a827
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2015-04-16 21:47:27 +0200

    Function for adding messages

--- scripts/web-chat.js
+++ scripts/web-chat.js
@@ -1,3 +1,18 @@
 (function () {
     console.log('Loaded web-chat.js');
+
+// TODO
+$('.chat-history').append (create_message_html ('imagod', '42:42:13', 'Lorem ipsum ... you know it already.. ;)'));
+$('.chat-history').append (create_message_html ('imyourfather', '42:42:14', 'Test entry'));
+
+    function create_message_html (author, time, message) {
+        var html = "<div class=\"message\">";
+
+        html += "<span class=\"irc-username " + author + "\">" + author + "</span>";
+        html += "<span class=\"irc-timestamp\">" + time + "</span>";
+        html += "<p class=\"message-text\">" + message + "</p>";
+        html += "</div>";
+
+        return html;
+    }
 })();
--- web-chat.php
+++ web-chat.php
@@ -13,18 +13,7 @@
                 <h4>Ask the elementary-developers your questions</h4>
             </div>
 
-            <div class="row chat-history">
-                <div class="message">
-                    <span class="irc-username someuser">someuser</span>
-                    <span class="irc-timestamp">7:00 pm</span>
-                    <p class="message-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-                </div>
-                <div class="message">
-                    <span class="irc-username imyourfather">imyourfather</span>
-                    <span class="irc-timestamp">7:02 pm</span>
-                    <p class="message-text">At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-                </div>
-            </div>
+            <div class="row chat-history"></div>
 
             <div class="row">
                 <input class="chat-input" type="text" />
